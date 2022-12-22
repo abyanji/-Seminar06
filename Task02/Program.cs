d0ef1f03fa8239553791d2a508edea8cb893001f
abyanji <a.byanjil99@gmail.com>
2022-12-22 18:30:18 +0300

    Task02 & Task03

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void TriangleCheck(int sideOne, int sideTwo, int sideThree)
+{
+    if (sideOne < sideTwo + sideThree && sideTwo < sideOne + sideThree && sideThree < sideOne + sideTwo)
+    {
+        System.Console.WriteLine("Такой треугольник может существовать");
+    }
+    else
+    {
+        System.Console.WriteLine("Такой треугольник НЕ может существовать");
+    }
+}
+
+int firstSide = DataEntry("Введите первую сторону треугольника: ");
+int secondSide = DataEntry("Введите вторую сторону треугольника: ");
+int thirdSide = DataEntry("Введите третью сторону треугольника: ");
+TriangleCheck(firstSide, secondSide, thirdSide);
---
+++ Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void ToBinary(int num)
+{
+    if(num <= 0)
+    {
+        return;
+    }
+    ToBinary(num / 2);
+    System.Console.Write(num % 2);
+}
+
+int number = DataEntry("введите десятичное число: ");
+ToBinary(number);
