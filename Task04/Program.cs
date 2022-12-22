090670826878300367b22eb2e4247d9ca82662e6
abyanji <a.byanjil99@gmail.com>
2022-12-22 19:16:05 +0300

    Task4 & Task05

---
+++ Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void Fibonachi(int num)
+{
+    int[] arr = new int[num];
+    arr[0] = 0;
+    arr[1] = 1;
+    int i;
+    for(i = 2; i < num; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    Console.WriteLine($"[{string.Join(", ", arr)}]");
+}
+
+int number = DataEntry("введите число: ");
+Fibonachi(number);
---
+++ Task05/Program.cs
@@ -0,0 +1,32 @@
+﻿const int SIZE = 8;
+const int LEFT_RANGE = 0;
+const int RIGHT_RANGE = 100;
+
+int[] array = new int[SIZE];
+int[] array2 = new int[SIZE];
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for(int i = 0 ; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] arr2 = new int[arr.Length];
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        arr2[i] = arr[i];
+    }
+    return arr2;
+}
+
+array = FillArray(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+array2 = CopyArray(array);
+Console.WriteLine($"[{string.Join(", ", array2)}]");
